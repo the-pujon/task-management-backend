9c7b5ac38616a54099bc5dd89e1f89a8248a45fc
Pujon Das Auvi <auvipujondas@gmail.com>
2023-11-26 23:58:19 +0600

    done

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const taskController = require("../controllers/task.controller");
 const authMiddleware = require("../middleware/auth.middleware");
+router.use(authMiddleware);
 
 // Create a new task
 router.post("/", taskController.createTask);
